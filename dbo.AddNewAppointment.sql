7f811fbe00614814827262e610b1116c6d802548
nishtha123456 <108168210+nishtha123456@users.noreply.github.com>
2023-06-10 10:36:46 +0530

    Add files via upload

---
+++ dbo.AddNewAppointment.sql
@@ -0,0 +1,11 @@
+﻿Create procedure [dbo].[AddNewAppointment]  
+(  
+ @Pid int,
+   @Pname nvarchar (50),  
+   @AppointmentDate Date,  
+   @Dname nvarchar (100)  
+)  
+as  
+begin  
+   Insert into Appointment values(@Pid,@Pname,@AppointmentDate,@Dname)  
+End
